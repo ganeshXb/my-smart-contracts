cfcbc717b16e1f7e9ce185456de517d8d1a4d5c7
Ganeshb62 <ganesh622k@icloud.com>
2021-07-21 23:40:31 +0530

    Add crowdfunding contracts

---
+++ crowdfunding/contracts/CrowdFunding.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0;
+
+contract CrowdFunding {
+
+    string public name;
+
+    constructor() public{
+        name = "Ganesh's CrowdFunding";
+    }
+    
+    // List all the exsisting projects
+
+}
